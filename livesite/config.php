64757c93316ad86fceb69dc20933081f829162ba
Donna <dholmes@kyanicorp.com>
2015-08-19 21:37:25 -0600

    livesite backup, changing link to login

---
+++ livesite/config.php
@@ -0,0 +1,8 @@
+<?php
+define('DB_HOST', 'localhost');
+define('DB_USERNAME', 'ipawdspo_lvs1');
+define('DB_PASSWORD', 'GFBaL6eP3U');
+define('DB_DATABASE', 'ipawdspo_lvs1');
+define('LICENSE_KEY', '2qaczZ+LieByYFaebZaP5aKMh8moX1SVa1qSzbOPiNSgm1uQbV9W3NWzi8fFoJGhaw==');
+define('ENCRYPTION_KEY', 'EZTZYDHEH4ZJFMY6TWLR8ZT5CLKO1SGP'); // DO NOT MODIFY OR SHARE
+?>
